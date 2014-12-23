f0d8769afef28d7360f66d13ccf1c372d9a336d7
Lucas Gonze <lucas@gonze.com>
2014-12-23 13:09:38 -0800

    Keep configs out of repo

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 
-# 12-factor apps keep config params out of the repo. This file is for the 
+# 12-factor apps keep config params out of the repo. This file is for the config params.
+
 # 1. Get an app ID at http://www.openaura.com/apps/new
 # 2. Set it below
-#YOUR_API_KEY=this value is a stub
+YOUR_API_KEY=65cdb7d0dbf8263708aab36a9c4b64d95a0b35b5
